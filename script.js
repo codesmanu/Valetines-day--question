b7bf8016c7b602d2535e022d3a999f830d657de5
codesmanu <contact.rackel@gmail.com>
2025-02-13 22:48:34 -0300

    adição música e ajustes do js

--- script.js
+++ script.js
@@ -1,39 +1,69 @@
-/* style.css */
+function selectOption(option) {
+    if (option === 'yes') {
+        flashRainbowColors(function () {
+            document.getElementById('question').style.display = 'none';
+            displayCatHeart();
+            hideSecondQuestion();
+            playBackgroundMusic();
+        });
+    } else if (option === 'no') {
+        document.getElementById('no-button').innerText = 'You sure?';
 
-body {
-    font-family: Arial, sans-serif;
-    text-align: center; 
-    margin: 0; 
-    background-color: #FADADD; 
+        var yesButton = document.getElementById('yes-button');
+        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+        var newSize = parseFloat(currentFontSize) * 2;
+        yesButton.style.fontSize = newSize + 'px';
+    } else {
+        alert('Invalid option!');
+    }
 }
 
-#container {
-    display: flex; 
-    flex-direction: column; 
-    align-items: center; 
-    justify-content: center; 
-    height: 100vh; 
+function flashRainbowColors(callback) {
+    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var i = 0;
+    var interval = setInterval(function () {
+        document.body.style.backgroundColor = colors[i];
+        i = (i + 1) % colors.length;
+    }, 200);
+    setTimeout(function () {
+        clearInterval(interval);
+        document.body.style.backgroundColor = '';
+        if (callback) {
+            callback();
+        }
+    }, 2000);
 }
 
-#image-container {
+function displayCat() {
+    var imageContainer = document.getElementById('image-container');
+    var catImage = new Image();
+    catImage.src = 'cat.gif';
+    catImage.alt = 'Cat';
+    catImage.onload = function () {
+        imageContainer.appendChild(catImage);
+    };
 }
 
-#question {
-    font-family: 'Sacramento', cursive; 
-    font-size: 52px; 
+function displayCatHeart() {
+    document.getElementById('image-container').innerHTML = '';
+
+    var imageContainer = document.getElementById('image-container');
+    var catHeartImage = new Image();
+    catHeartImage.src = 'cat-heart.gif';
+    catHeartImage.alt = 'Cat Heart';
+    catHeartImage.onload = function () {
+        imageContainer.appendChild(catHeartImage);
+        document.getElementById('options').style.display = 'none';
+    };
 }
 
-#options {
-    margin-top: 20px; 
+function hideSecondQuestion() {
+    document.getElementById('second-question').style.display = 'none';
 }
 
-button {
-    padding: 10px 20px; 
-    margin: 0 10px; 
-    font-size: 26px;
-    font-family: 'Sacramento', cursive; 
-    background-color: #FB607F; 
-    color: white;
-    border: none; 
-    cursor: pointer; 
+function playBackgroundMusic() {
+    var audio = new Audio('somesoyou.mp3');
+    audio.play();
 }
+
+displayCat();
